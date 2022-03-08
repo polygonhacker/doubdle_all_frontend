50e866d6499fe479bdf59d7e205a6e64a4b7832c
Mark Park <mark.dm.park@gmail.com>
2022-03-07 22:37:47 -0800

    double started navbar and main screen

---
+++ src/components/MainScreen/MainScreen.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import './MainScreen.scss'
+
+const MainScreen = () => {
+    return (
+        <section className='main'>
+            <div className='container 1'>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+            </div>
+
+            <div className='container 2'>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MainScreen;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './NavBar.scss';
+
+const NavBar = () => {
+
+    return (
+        <header className='nav-bar'>
+            <div className='container'>
+                <div className='left-menu'>
+                    <div>
+                        something
+                    </div>
+                </div>
+                <h1>DOUBDLE</h1>
+                <div className='right-menu'>
+                    XYZ
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar;
