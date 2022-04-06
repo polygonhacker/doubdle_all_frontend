10f0dade5914b99caa185cd52d8b87fac2fd1994
Mark Park <mark.dm.park@gmail.com>
2022-04-05 22:52:56 -0700

    the help modal has clear instructions

--- src/components/HelpModal/HelpModal.tsx
+++ src/components/HelpModal/HelpModal.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import ReactDOM from 'react-dom';
 import { HelpModalContext } from "../../contexts/helpModalContext";
-import {ReactComponent as Close} from '../../assets/remove.svg';
+import { ReactComponent as Close } from '../../assets/remove.svg';
+import { green, yellow, grey } from '../../assets/colors';
 import './HelpModal.css';
 
 
@@ -16,15 +17,27 @@ const HelpModal = () => {
     return ReactDOM.createPortal(
         <>
             <div className="modal-container">
-                <Close className="close-button" onClick={() => {setHelpModal(!helpModal)}} />
+                <div className="help-header">
+                    <p>Instructions</p>
+                    <Close className="close-button" onClick={() => {setHelpModal(!helpModal)}} />
+                </div>
+                
                 <p>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
-                    Here are the instructions.<br/>Herearethe instructions.<br/>
+                    <br/>
+                    Try to guess two five-letter words in six tries.
+                    <br/>
+                    <br/>
+                    Each guess must be a valid five-letter word. Hit the enter button to submit.
+                    <br/>
+                    <br/>
+                    After each guess, the color of the tiles will change to show how close your guess was to the word.
+                    <br/>
+                    <br/>
+                    <span style={{color: green, fontWeight: 'bold'}}>Green</span>: the letter is in the correct place.
+                    <br/>
+                    <span style={{color: yellow, fontWeight: 'bold'}}>Yellow</span>: the answer contains the letter but is not in the correct place.
+                    <br/>
+                    <span style={{color: grey, fontWeight: 'bold'}}>Grey</span>: the answer does not contain the letter.
                 </p>
                 
             </div>
